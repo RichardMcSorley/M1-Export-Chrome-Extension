63fdf9b32f4476b049268f42d40dc7f1ef0be153
Richard McSorley <Richard.McSorley@hey.com>
2020-07-18 15:46:20 -0500

    Export all Columns

--- background.js
+++ background.js
@@ -1,30 +1,31 @@
-const documentUrlPatterns = ["https://dashboard.m1finance.com/d/invest/holdings"];
+const documentUrlPatterns = [
+    "https://dashboard.m1finance.com/d/invest/holdings"
+];
 
-function send(type){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage( tabs[0].id, type);
+function send(type) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, type);
     });
 }
 
 chrome.contextMenus.create({
     id: "m1ToCSV",
-    title: 'Copy/Save Holdings',
-    documentUrlPatterns,
+    title: "Copy/Save Holdings",
+    documentUrlPatterns
 });
 
 chrome.contextMenus.create({
     id: "m1ToCSV-save",
     parentId: "m1ToCSV",
-    title: 'Save to CSV',
-    onclick: ()=>send('save'),
-    documentUrlPatterns,
+    title: "Save to CSV",
+    onclick: () => send("save"),
+    documentUrlPatterns
 });
 
 chrome.contextMenus.create({
     id: "m1ToCSV-copy",
     parentId: "m1ToCSV",
-    title: 'Copy to clipboard',
-    onclick: ()=>send('copy'),
-    documentUrlPatterns,
+    title: "Copy to clipboard",
+    onclick: () => send("copy"),
+    documentUrlPatterns
 });
-
--- content.js
+++ content.js
@@ -1,77 +1,102 @@
 chrome.runtime.onMessage.addListener(function (request) {
-    if(request === 'save') save();
-    if(request === 'copy') copy();
+    if (request === "save") save();
+    if (request === "copy") copy();
 });
 
-function getData(){
+function getData() {
     const commaRows = [];
     const tabbedRows = [];
+    // First 3 lines of headers should not move, the order is to support simply safe dividends
     const headers = [
-            "Ticker",
-            "Quantity",
-            "Cost Per Share",
-        ];
-    
-    try{
-        
-        const elmRows = document.querySelectorAll('a[href*="position"]');
-        if(!elmRows) throw('no rows');
+        "Ticker",
+        "Shares",
+        "Avg. Price",
+        "Name",
+        "Maintenance",
+        "Unrealized Gain",
+        "Unrealized Gain %",
+        "Value"
+    ];
 
-        elmRows.forEach((elm)=>{
+    try {
+        // Find row elements -> find values in each row -> extract text
+        const rows = [
+            ...document.querySelectorAll('a[href*="position"]')
+        ].map(elm =>
+            [...elm.querySelectorAll("span")].map(elm => elm.textContent)
+        );
+
+        rows.forEach(row => {
             const [
+                ticker, // MAIN
+                name, // Main Street Capital Corp.
+                shares, // 12.63455
+                price, // $30.32
+                maintenance, // 45%
+                gain, // +$18.31 (▲4.78%)
+                gain1, // +$18.31
+                gain2, // ▲4.78%
+                gain3, // ▲
+                gain4, // 4.78%
+                value // $401.40
+            ] = row;
+
+            const newRow = [
+                // First 3 lines  should not move, the order is to support simply safe dividends
                 ticker,
-                _,
                 shares,
                 price,
-            ] = elm.querySelectorAll('span');
-
-
-            commaRows.push([
-                ticker.textContent,
-                shares.textContent,
-                price.textContent,
-            ].join(','));
-
-            tabbedRows.push([
-                ticker.textContent,
-                shares.textContent,
-                price.textContent,
-            ].join('\t'));
-
+                name.replace(",", ""), // escape , for CSV. Ex. CompanyName , Inc.
+                maintenance,
+                gain1.replace("+", ""), // remove + value, most spreadsheet software interpret no value as positive
+                gain2.replace("▲", "").replace("▼", "-"), // replace symbols with positive or negative values
+                value
+            ];
+            commaRows.push(newRow.join(","));
+            tabbedRows.push(newRow.join("\t"));
         });
-
-    } catch(error){
+    } catch (error) {
         console.error(error);
-        commaRows.push(['Error getting holdings']);
-        tabbedRows.push(['Error getting holdings']);
+        commaRows.push(["Error getting holdings"]);
+        tabbedRows.push(["Error getting holdings"]);
     }
 
     return {
         headers,
         tabbedRows,
         commaRows
-    }
+    };
 }
 
-function save(){
+function save() {
     const { commaRows, headers } = getData();
-    const encodedUri = encodeURI(`data:text/csv;charset=utf-8,${headers.join(',')}\n${commaRows.join('\n')}`);
+    const encodedUri = encodeURI(
+        `data:text/csv;charset=utf-8,${headers.join(",")}\n${commaRows.join(
+            "\n"
+        )}`
+    );
     const link = document.createElement("a");
     link.setAttribute("href", encodedUri);
     link.setAttribute("download", "m1.csv");
-    document.body.appendChild(link); // Required for Firefox
-    link.click(); // This will download the file
+    document.body.appendChild(link);
+    link.click();
     link.remove();
-    console.log('Saved holdings to CSV.')
+    console.log("Saved holdings to CSV.");
 }
 
 function copy() {
     const { tabbedRows, headers } = getData();
-    navigator.clipboard.writeText(`${headers.join('\t')}\n${tabbedRows.join('\n')}`).then(() => {
-        console.log('Copied holdings into clipboard.')
-    }, (e) => {
-        console.log('Could not copy holdings into clipboard.', e)
-        alert('Could not copy holdings into clipboard. Please try again.')
-    });
+    navigator.clipboard
+        .writeText(`${headers.join("\t")}\n${tabbedRows.join("\n")}`)
+        .then(
+            () => {
+                console.log("Copied holdings into clipboard.");
+            },
+            e => {
+                console.log("Could not copy holdings into clipboard.", e);
+                alert(
+                    "Could not copy holdings into clipboard. Please try again."
+                );
+            }
+        );
 }
-
